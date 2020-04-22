6bfc484e561bb93a2d5b8314a659f8b74f7762ed
Matt Liu <414527086@qq.com>
2020-04-22 11:42:11 +0800

    199. Binary Tree Right Side View

---
+++ 199. Binary_Tree_Right_Side_View.go
@@ -0,0 +1,41 @@
+package main
+
+
+// Given a binary tree, imagine yourself standing on the right side of it,
+// return the values of the nodes you can see ordered from top to bottom.
+
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+func rightSideView(root *TreeNode) []int {
+	if root == nil{
+		return nil
+	}
+	if root.Left==nil && root.Right==nil{
+		return []int{root.Val}
+	}else {
+		leftView := rightSideView(root.Left)
+		rightView := rightSideView(root.Right)
+		return append([]int{root.Val},merge(leftView,rightView)...)
+	}
+}
+
+func merge(left []int,right []int)[]int  {
+	var result []int
+	if len(left)>len(right){
+		result = make([]int,len(left))
+	}else {
+		result = make([]int,len(right))
+	}
+	for i:=0;i<len(result);i++{
+		if i<len(right){
+			result[i] = right[i]
+		}else {
+			result[i] = left[i]
+		}
+	}
+	return result
+}
